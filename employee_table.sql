f126818c21863198c2637d67ec8e3ce953000f38
agarcia50S <agarcia50@luc.edu>
2023-02-10 13:15:50 -0600

    relate super_id to employee_id

--- employee_table.sql
+++ employee_table.sql
@@ -8,3 +8,8 @@ CREATE TABLE employee (
     super_id INT, -- will point to employee_id
     branch_id INT -- will point to branch_table branch_id
 );
+
+-- relating super_id to employee_id
+ALTER TABLE employee ADD FOREIGN KEY(super_id)
+-- when employee_id value removed, add null to super_id
+REFERENCES employee(employee_id) ON DELETE SET NULL
