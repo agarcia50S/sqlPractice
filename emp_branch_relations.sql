f4870382a1af47db59c10dcd941456a8f0df38c6
agarcia50S <agarcia50@luc.edu>
2023-02-10 20:29:33 -0600

    point employee and branch to each other

---
+++ emp_branch_relations.sql
@@ -0,0 +1,7 @@
+ALTER TABLE employee ADD FOREIGN KEY(branch_id)
+-- if branch.branch_id is deleted, add null to employee.branch_id 
+REFERENCES branch(branch_id) ON DELETE NULL;
+
+ALTER TABLE branch ADD FOREIGN KEY(mgr_id)
+-- if delete employee.employee_id, add null to branch.mgr_id
+REFERENCES employee(employee_id) ON DELETE NULL;
