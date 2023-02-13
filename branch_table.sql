ad9844928ee08a0ac7884881b4ceb2dfe569e1ff
agarcia50S <agarcia50@luc.edu>
2023-02-13 14:37:51 -0600

    auto increment branch_id column

--- branch_table.sql
+++ branch_table.sql
@@ -1,6 +1,6 @@
 -- base table; will be pointed at by and point to the employee table
 CREATE TABLE branch(
-    branch_id INT PRIMARY KEY,
+    branch_id INT PRIMARY KEY AUTO_INCREMENT,
     branch_name VARCHAR(30),
     mgr_id INT,
     mgr_start_date DATE
