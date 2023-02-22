2b85ad790cc76228c2e63579cfb15f3c4864801a
agarcia50S <agarcia50@luc.edu>
2023-02-21 22:10:34 -0600

    fix syntax; should be ON SET NULL not ON NULL

--- emp_branch_relations.sql
+++ emp_branch_relations.sql
@@ -1,7 +1,7 @@
 ALTER TABLE employee ADD FOREIGN KEY(branch_id)
 -- if branch.branch_id is deleted, add null to employee.branch_id 
-REFERENCES branch(branch_id) ON DELETE NULL;
+REFERENCES branch(branch_id) ON DELETE SET NULL;
 
 ALTER TABLE branch ADD FOREIGN KEY(mgr_id)
 -- if delete employee.employee_id, add null to branch.mgr_id
-REFERENCES employee(employee_id) ON DELETE NULL;
+REFERENCES employee(employee_id) ON DELETE SET NULL;
