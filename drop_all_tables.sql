92f57361d8d75a082d3d0efd55790f017065b712
agarcia50S <agarcia50@luc.edu>
2023-02-21 22:45:06 -0600

    disable/enable FK constraint to drop tables

--- drop_all_tables.sql
+++ drop_all_tables.sql
@@ -1,5 +1,7 @@
 DROP TABLE IF EXISTS works_with;
 DROP TABLE IF EXISTS supplier;
 DROP TABLE IF EXISTS client;
-DROP TABLE IF EXISTS branch;
+SET foreign_key_checks = 0;
 DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS branch;
+SET foreign_key_checks = 1;
